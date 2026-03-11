6e64a5b40def2114bef2c243d1ed5e6b6889d247
maxonkizino <mkizino83@gmail.com>
2026-03-11 17:27:27 +0300

    first

---
+++ src/test/java/com/javainternship/JavaInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
