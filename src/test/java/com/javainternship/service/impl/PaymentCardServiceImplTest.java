47c3dfc7209a7ada24a4f03978d4b262066ead0b
maxonkizino <mkizino83@gmail.com>
2026-03-15 17:48:05 +0300

    user-service:    feat: add profiles    feat: add containerization task:     #8 containerizing

--- src/test/java/com/javainternship/service/impl/PaymentCardServiceImplTest.java
+++ src/test/java/com/javainternship/service/impl/PaymentCardServiceImplTest.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +31,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+
+@ActiveProfiles("test")
 class PaymentCardServiceImplTest {
 
     @Mock
